d8835680c314802d0d090524987184e49add8b00
yeshealer <yeshealer@gmail.com>
2023-07-13 11:49:49 +0900

    update key props

--- components/PartnerSection/index.tsx
+++ components/PartnerSection/index.tsx
@@ -8,7 +8,7 @@ export default function PartnerSection() {
             <div className='text-yellow-100 text-3xl md:text-5xl font-bold tracking-widest my-4'>OUR PARTNERS</div>
             <div className='flex flex-wrap gap-5 justify-between'>
                 {PartnerDetails.map(item => (
-                    <div className='bg-gradient-to-r from-zinc-600/20 to-zinc-700/30 border border-yellow-600/20 p-1 sm:p-4 rounded-md backdrop-blur flex items-center justify-center w-full sm:w-2/5 md:w-auto'>
+                    <div key={item.name} className='bg-gradient-to-r from-zinc-600/20 to-zinc-700/30 border border-yellow-600/20 p-1 sm:p-4 rounded-md backdrop-blur flex items-center justify-center w-full sm:w-2/5 md:w-auto'>
                         <Image src={`/partner/${item.image}.png`} width={400} height={70} alt='partner logo' draggable={false} className='w-1/2 sm:w-[200px]' />
                     </div>
                 ))}
--- components/RoadmapSection/index.tsx
+++ components/RoadmapSection/index.tsx
@@ -8,14 +8,14 @@ export default function RoadmapSection() {
             <div className='text-yellow-100 text-3xl md:text-5xl font-bold tracking-widest my-4'>ROADMAP</div>
             <div className='flex flex-col gap-4'>
                 {RoadmapDetails.map(item => (
-                    <div className='bg-gradient-to-r from-zinc-600/20 to-zinc-700/30 border border-yellow-600/20 px-3 md:px-10 py-3 md:py-6 rounded-md backdrop-blur'>
+                    <div key={item.period} className='bg-gradient-to-r from-zinc-600/20 to-zinc-700/30 border border-yellow-600/20 px-3 md:px-10 py-3 md:py-6 rounded-md backdrop-blur'>
                         <div className='text-base md:text-xl text-yellow-500 tracking-wider font-semibold flex items-center gap-4'>
                             <div className='text-yellow-100 bg-yellow-500 w-6 h-6 md:w-8 md:h-8 rounded-full flex items-center justify-center'>{item.id}</div>
                             {item.period}
                         </div>
                         <div className='pl-6 md:pl-12'>
                             {item.details.map(detail => (
-                                <div className='tracking-wider text-sm text-yellow-200 opacity-70 flex items-start gap-2 mt-1'>
+                                <div key={detail} className='tracking-wider text-sm text-yellow-200 opacity-70 flex items-start gap-2 mt-1'>
                                     <Image src={'/bg-logo.png'} width={16} height={12} alt='background logo' draggable={false} className='mt-0.5' />
                                     {detail}
                                 </div>
